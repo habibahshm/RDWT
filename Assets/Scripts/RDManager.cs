17f1ec3838d4c4f8c2d09ef1b36edc0d20d3ed11
Habiba Hesham <habibahshm@gmail.com>
2023-07-03 17:16:13 +0900

    calculated rotation and curvature gains

--- Assets/Scripts/RDManager.cs
+++ Assets/Scripts/RDManager.cs
@@ -7,22 +7,13 @@ using UnityEngine;
 
 public class RDManager : MonoBehaviour
 {
-
-    [Tooltip("Maximum translation gain applied")]
-    [Range(0, 5)]
-    public float MAX_TRANS_GAIN = 0.26F;
-
-    [Tooltip("Minimum translation gain applied")]
-    [Range(-0.99F, 0)]
-    public float MIN_TRANS_GAIN = -0.14F;
-
     [Tooltip("Maximum rotation gain applied")]
-    [Range(0, 5)]
-    public float MAX_ROT_GAIN = 0.49F;
+    [Range(1F, 2F)]
+    public float MAX_ROT_GAIN = 1.3F;
 
     [Tooltip("Minimum rotation gain applied")]
-    [Range(-0.99F, 0)]
-    public float MIN_ROT_GAIN = -0.2F;
+    [Range(0.1F, 1F)]
+    public float MIN_ROT_GAIN = 0.85F;
 
     [Tooltip("Radius applied by curvature gain")]
     [Range(1, 23)]
@@ -31,6 +22,7 @@ public class RDManager : MonoBehaviour
     [Tooltip("The game object that is being physically tracked (probably user's head)")]
     public Transform headTransform;
 
+  
     public TextMeshProUGUI debugUI;
 
     [HideInInspector]
@@ -40,13 +32,18 @@ public class RDManager : MonoBehaviour
     public Vector3 center; // center of the tracking area
 
     [HideInInspector]
-    public Vector3 currPos; //cur pos of user w.r.t the OVR rig which is aligned with the (0,0,0)
+    public Vector3 currPos, prevPos, currDir, prevDir; //cur pos of user w.r.t the OVR rig which is aligned with the (0,0,0)
 
     [HideInInspector]
-    public Vector3 currDir; // cur forward direction of the user
+    public Vector3 deltaPos;
+    [HideInInspector]
+    public float deltaDir;
 
-    [SerializeField] GameObject userDir;
-    [SerializeField] GameObject dirTocenter;
+    enum RotationObject { UserHead, Env };
+
+    [SerializeField] GameObject userDirVector;
+    [SerializeField] GameObject dirTocenterVector;
+    [SerializeField] RotationObject ObjectToRotate;
 
     private const float S2C_BEARING_ANGLE_THRESHOLD_IN_DEGREE = 160;
     private const float S2C_TEMP_TARGET_DISTANCE = 4;
@@ -54,6 +51,15 @@ public class RDManager : MonoBehaviour
     private bool no_tmptarget = true;
     private Vector3 tmp_target;       // the curr redirection target
 
+    private const float MOVEMENT_THRESHOLD = 0.2f; // meters per second
+    private const float ROTATION_THRESHOLD = 1.5f; // degrees per second
+    private const float CURVATURE_GAIN_CAP_DEGREES_PER_SECOND = 15;  // degrees per second
+    private const float ROTATION_GAIN_CAP_DEGREES_PER_SECOND = 30;  // degrees per second
+
+    // Auxiliary Parameters
+    private float rotationFromCurvatureGain; //Proposed curvature gain based on user speed
+    private float rotationFromRotationGain; //Proposed rotation gain based on head's yaw
+    private float lastRotationApplied = 0f;
 
     void Update()
     {
@@ -62,18 +68,60 @@ public class RDManager : MonoBehaviour
             return;
         }
 
-        currPos = Utilities.FlattenedPos3D(headTransform.position);
-        currDir = Utilities.FlattenedDir3D(headTransform.forward);
+        UpdateCurrentUserState();
+        CalculateDelta();
 
-        LineRenderer lineRenderer = dirTocenter.GetComponent<LineRenderer>();
+        LineRenderer lineRenderer = dirTocenterVector.GetComponent<LineRenderer>();
         lineRenderer.SetPosition(0, currPos);
         lineRenderer.SetPosition(1, center);
 
-        LineRenderer lineRenderer2 = userDir.GetComponent<LineRenderer>();
+        LineRenderer lineRenderer2 = userDirVector.GetComponent<LineRenderer>();
         lineRenderer2.SetPosition(0, currPos);
         lineRenderer2.SetPosition(1, Utilities.FlattenedPos3D(headTransform.TransformPoint(Vector3.forward * 0.5f)));
-        
+
+        ApplyRedirection();
+
+        UpdatePreviousUserState();
+
+    }
+
+    public void ApplyRedirection()
+    {
         S2C_PickRedirectionTarget();
+
+        rotationFromCurvatureGain = 0;
+
+        if((deltaPos.magnitude/ Time.deltaTime) > MOVEMENT_THRESHOLD)
+        {
+            rotationFromCurvatureGain = Mathf.Rad2Deg * (deltaPos.magnitude / CURVATURE_RADIUS);
+            rotationFromCurvatureGain = Mathf.Min(rotationFromCurvatureGain, CURVATURE_GAIN_CAP_DEGREES_PER_SECOND)* Time.deltaTime;
+        }
+
+        //Compute desired facing vector for redirection
+        Vector3 desiredFacingDirection = Utilities.FlattenedPos3D(redirection_target) - currPos;
+        int signOfAngle = (int)Mathf.Sign(Utilities.GetSignedAngle(currDir, desiredFacingDirection));
+        int desiredSteeringDirection = ObjectToRotate == RotationObject.UserHead ? (-1) * signOfAngle : signOfAngle;
+
+        //Compute proposed rotation gain
+        rotationFromRotationGain = 0;
+
+        if (Mathf.Abs(deltaDir) / Time.deltaTime >= ROTATION_THRESHOLD)
+        {
+            //Determine if we need to rotate with or against the user
+            if ((deltaDir * desiredSteeringDirection < 0 && ObjectToRotate == RotationObject.UserHead) || (deltaDir * desiredSteeringDirection > 0 && ObjectToRotate == RotationObject.Env))
+            {
+                //Rotating against the user
+                rotationFromRotationGain = Mathf.Min(Mathf.Abs(deltaDir * MIN_ROT_GAIN), ROTATION_GAIN_CAP_DEGREES_PER_SECOND) * Time.deltaTime; 
+            }
+            else
+            {  
+                //Rotating with the user
+                rotationFromRotationGain = Mathf.Min(Mathf.Abs(deltaDir * MAX_ROT_GAIN), ROTATION_GAIN_CAP_DEGREES_PER_SECOND) * Time.deltaTime;
+            }
+        }
+
+        
+
 
     }
 
@@ -92,7 +140,7 @@ public class RDManager : MonoBehaviour
             {
                 if (no_tmptarget)
                 {
-                    tmp_target = currPos + S2C_TEMP_TARGET_DISTANCE * (Quaternion.Euler(0, Utilities.GetSignOfAngle(currDir, userToCenter) * 90, 0) * currDir);
+                    tmp_target = currPos + S2C_TEMP_TARGET_DISTANCE * (Quaternion.Euler(0, (int)Mathf.Sign(signedAngle) * 90, 0) * currDir);
                     no_tmptarget = false;
                 }
                 redirection_target = tmp_target;
@@ -104,5 +152,23 @@ public class RDManager : MonoBehaviour
             }
 
         }
+    }
+
+    void UpdateCurrentUserState()
+    {
+        currPos = Utilities.FlattenedPos3D(headTransform.position);
+        currDir = Utilities.FlattenedDir3D(headTransform.forward);
+    }
+
+    void UpdatePreviousUserState()
+    {
+        prevPos = Utilities.FlattenedPos3D(headTransform.position);
+        prevDir = Utilities.FlattenedDir3D(headTransform.forward);
+    }
+
+    void CalculateDelta()
+    {
+        deltaPos = currPos - prevPos;
+        deltaDir = Utilities.GetSignedAngle(prevDir, currDir);
     }
 }
